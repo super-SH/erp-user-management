657bedaf6b70ce891483eb35e02339502ab48432
super-SH <dev.swam.htet@gmail.com>
2024-02-23 01:51:27 +0630

    feat: structure basic route layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold tracking-tight md:text-5xl lg:text-6xl lg:tracking-tighter '>
+        Welcome to PicoSBS
+      </h1>
+      <p className='max-w-[600px]  text-gray-700 md:text-xl dark:text-gray-400'>
+        We build the tools you need to succeed.
+      </p>
+    </>
+  );
+}
---
+++ app/roles/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Page() {
+  return <div>Roles</div>;
+}
+
+export default Page;
