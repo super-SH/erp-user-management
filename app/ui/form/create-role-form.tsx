c5554345fd19b5c434b783a54c5ddccd8017533a
super-SH <dev.swam.htet@gmail.com>
2024-02-24 02:41:10 +0630

    feat: add adminstrator access checkbox

--- app/ui/form/create-role-form.tsx
+++ app/ui/form/create-role-form.tsx
@@ -21,68 +21,68 @@ import { Separator } from '@/components/ui/separator';
 
 // TODO:
 // This will be fetch from the database. This is a dummy placeholder.
-// const featureActions = [
-//   {
-//     id: 1,
-//     feature: 'User',
-//     actionType: 'Create',
-//   },
-//   {
-//     id: 2,
-//     feature: 'User',
-//     actionType: 'Read',
-//   },
-//   {
-//     id: 3,
-//     feature: 'User',
-//     actionType: 'Update',
-//   },
-//   {
-//     id: 4,
-//     feature: 'User',
-//     actionType: 'Delete',
-//   },
-//   {
-//     id: 5,
-//     feature: 'Role',
-//     actionType: 'Create',
-//   },
-//   {
-//     id: 6,
-//     feature: 'Role',
-//     actionType: 'Read',
-//   },
-//   {
-//     id: 7,
-//     feature: 'Role',
-//     actionType: 'Update',
-//   },
-//   {
-//     id: 8,
-//     feature: 'Role',
-//     actionType: 'Delete',
-//   },
-//   {
-//     id: 9,
-//     feature: 'Contact',
-//     actionType: 'Create',
-//   },
-//   {
-//     id: 10,
-//     feature: 'Contact',
-//     actionType: 'Read',
-//   },
-//   {
-//     id: 11,
-//     feature: 'Contact',
-//     actionType: 'Update',
-//   },
-//   {
-//     id: 12,
-//     feature: 'Contact',
-//     actionType: 'Delete',
-//   },
-// ];
+const featureActionsOrg = [
+  {
+    id: 1,
+    feature: 'User',
+    actionType: 'Create',
+  },
+  {
+    id: 2,
+    feature: 'User',
+    actionType: 'Read',
+  },
+  {
+    id: 3,
+    feature: 'User',
+    actionType: 'Update',
+  },
+  {
+    id: 4,
+    feature: 'User',
+    actionType: 'Delete',
+  },
+  {
+    id: 5,
+    feature: 'Role',
+    actionType: 'Create',
+  },
+  {
+    id: 6,
+    feature: 'Role',
+    actionType: 'Read',
+  },
+  {
+    id: 7,
+    feature: 'Role',
+    actionType: 'Update',
+  },
+  {
+    id: 8,
+    feature: 'Role',
+    actionType: 'Delete',
+  },
+  {
+    id: 9,
+    feature: 'Contact',
+    actionType: 'Create',
+  },
+  {
+    id: 10,
+    feature: 'Contact',
+    actionType: 'Read',
+  },
+  {
+    id: 11,
+    feature: 'Contact',
+    actionType: 'Update',
+  },
+  {
+    id: 12,
+    feature: 'Contact',
+    actionType: 'Delete',
+  },
+];
 
 const featureActions = {
   User: [
@@ -203,17 +203,66 @@ function CreateRoleForm() {
                 Role permissions
               </FormLabel>
 
+              {/* Administrator Access  */}
+              <div
+                className='grid grid-cols-[144px_1fr] gap-x-2 '
+                key={'adminPermission'}
+              >
+                <FormLabel className='text-lg font-semibold text-wrap'>
+                  Administrator Access
+                </FormLabel>
+
+                <FormField
+                  key={`adminPermission`}
+                  control={form.control}
+                  name='rolePermissions'
+                  render={({ field }) => (
+                    <FormItem
+                      key={`adminPermission`}
+                      className='flex flex-row items-center space-x-2 space-y-0'
+                    >
+                      <FormControl>
+                        <Checkbox
+                          className='size-6'
+                          checked={featureActionsOrg.every((permission) =>
+                            field.value.includes(permission.id)
+                          )}
+                          onCheckedChange={(checked) => {
+                            const ids = featureActionsOrg.map(
+                              (item) => item.id
+                            );
+                            field.onChange(
+                              checked
+                                ? [...field.value, ...ids]
+                                : field.value.filter((id) => !ids.includes(id))
+                            );
+                          }}
+                        />
+                      </FormControl>
+                      <FormLabel className='text-lg font-medium'>
+                        Select All
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* Separator between Administrator Access  and other role permissions */}
+              <Separator className='my-4' />
+
+              {/* Loop throught the featureAction ,whcih is fetched. Each Keys is an feature and has the value of array of actions(CRUD, import , print) corresponding to this feature  */}
               {Object.keys(featureActions).map((feature) => (
                 <React.Fragment key={feature}>
                   <div
-                    className='grid grid-cols-[144px_1fr] gap-x-2 '
+                    className='grid grid-cols-1 md:grid-cols-[144px_1fr] gap-x-2 gap-y-2 '
                     key={feature}
                   >
                     <FormLabel className='text-lg font-semibold overflow-hidden text-ellipsis'>
                       {feature}
                     </FormLabel>
 
-                    <div className='flex gap-4 justify-start  items-center'>
+                    {/* This is the selectAll Checkbox of this feature */}
+                    <div className='flex gap-4 justify-start flex-wrap  items-center'>
                       <FormField
                         key={`${feature}_selectAll`}
                         control={form.control}
@@ -249,6 +298,8 @@ function CreateRoleForm() {
                           </FormItem>
                         )}
                       />
+
+                      {/* Loop through the array of action of each featuer and display checkbox. */}
                       {/* FIXME: need to fix typescript error */}
                       {featureActions[feature].map((item) => (
                         <FormField
@@ -287,6 +338,7 @@ function CreateRoleForm() {
                     </div>
                   </div>
 
+                  {/* Seperator between each feature */}
                   <Separator className='my-4' />
                 </React.Fragment>
               ))}
