d0a1f3af369bf065b4119289655908e92fe65b4c
super-SH <dev.swam.htet@gmail.com>
2024-02-28 05:27:33 +0630

    feat: add filter user btn for small screen

--- app/users/(user-list)/data-table-toolbar.tsx
+++ app/users/(user-list)/data-table-toolbar.tsx
@@ -21,7 +21,7 @@ export function DataTableToolbar<TData>({
   const isFiltered = table.getState().columnFilters.length > 0;
 
   return (
-    <div className='flex items-center justify-between gap-2'>
+    <div className='flex flex-col md:flex-row justify-between gap-3 md:items-center py-4'>
       <div className='flex flex-1 items-center space-x-2'>
         <Input
           placeholder='Search username...'
@@ -60,14 +60,42 @@ export function DataTableToolbar<TData>({
           )}
         </div>
       </div>
-      <Button asChild>
-        <Link
-          href={'/users/create'}
-          className='flex justify-between items-center gap-2 w-fit'
-        >
-          <PlusIcon className='h-4 w-4 invert' /> Create User
-        </Link>
-      </Button>
+      <div className='flex flex-col sm:flex-row gap-3 sm:gap-5'>
+        <div className='flex md:hidden gap-2 flex-wrap'>
+          {table.getColumn('role') && (
+            <DataTableFacetedFilter
+              column={table.getColumn('role')}
+              title='Role'
+              options={roles}
+            />
+          )}
+          {table.getColumn('isActive') && (
+            <DataTableFacetedFilter
+              column={table.getColumn('isActive')}
+              title='Account status'
+              options={isActive}
+            />
+          )}
+          {isFiltered && (
+            <Button
+              variant='ghost'
+              onClick={() => table.resetColumnFilters()}
+              className='h-8 px-2 lg:px-3'
+            >
+              Reset
+              <CrossIcon className='ml-2 h-4 w-4' />
+            </Button>
+          )}
+        </div>
+        <Button asChild>
+          <Link
+            href={'/users/create'}
+            className='flex justify-between items-center gap-2 w-fit'
+          >
+            <PlusIcon className='h-4 w-4 invert' /> Create User
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
