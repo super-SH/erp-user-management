5dafba8bd7651cce947db352eba9bae4848484d4
super-SH <dev.swam.htet@gmail.com>
2024-02-29 21:16:21 +0630

    feat: add pagination limit and page count in user table

---
+++ app/users/(user-list)/data-table-pagiantion.tsx
@@ -0,0 +1,70 @@
+import { Table } from '@tanstack/react-table';
+
+import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { LeftArrowIcon, RightArrowIcon } from '@/app/ui/icons';
+
+interface DataTablePaginationProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTablePagination<TData>({
+  table,
+}: DataTablePaginationProps<TData>) {
+  return (
+    <div className='flex items-center justify-end px-2 py-4'>
+      <div className='flex items-center gap-6 lg:gap-8'>
+        <div className='flex items-center gap-2'>
+          <p className='text-sm font-medium'>Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className='h-8 w-[70px]'>
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side='top'>
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className='flex w-[100px] items-center justify-center text-sm font-medium'>
+          Page {table.getState().pagination.pageIndex + 1} of{' '}
+          {table.getPageCount()}
+        </div>
+        <div className='flex items-center gap-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className='sr-only'>Go to previous page</span>
+            <LeftArrowIcon className='h-5 w-5' />
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className='sr-only'>Go to next page</span>
+            <RightArrowIcon className='h-5 w-5' />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/users/(user-list)/data-table.tsx
+++ app/users/(user-list)/data-table.tsx
@@ -11,13 +11,11 @@ import {
   useReactTable,
 } from '@tanstack/react-table';
 
-import { Button } from '@/components/ui/button';
-
-import { LeftArrowIcon, RightArrowIcon } from '@/app/ui/icons';
 import { useState } from 'react';
 
 import CommonDataTable from '@/app/ui/common-data-table';
 import { DataTableToolbar } from './data-table-toolbar';
+import { DataTablePagination } from './data-table-pagiantion';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -49,33 +47,13 @@ export function DataTable<TData, TValue>({
   return (
     <div className='rounded-md bg-slate-100 dark:bg-slate-800'>
       {/* Operations like filter , create new role */}
-
       <DataTableToolbar table={table} />
-
       {/* Table , which display data */}
       <div>
         <CommonDataTable table={table} columns={columns} />
       </div>
-
       {/* Pagination */}
-      <div className='flex items-center justify-end space-x-2 py-4'>
-        <Button
-          variant='outline'
-          size='sm'
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          <LeftArrowIcon className='h-5 w-5' />
-        </Button>
-        <Button
-          variant='outline'
-          size='sm'
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          <RightArrowIcon className='h-5 w-5' />
-        </Button>
-      </div>
+      <DataTablePagination table={table} />
     </div>
   );
 }
